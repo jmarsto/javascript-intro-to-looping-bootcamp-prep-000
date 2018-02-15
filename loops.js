4fdefd49b60a03e94a75d012edbfab1389dd5d85
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-15 22:11:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -23,7 +23,7 @@ function maybeTrue() {
 
 function doWhileLoop(array) {
   do {
-    array.length = (array.length - 1)
+    array.length = array.length - 1
   } while (array.length > 0 && maybeTrue())
   return array
 }
