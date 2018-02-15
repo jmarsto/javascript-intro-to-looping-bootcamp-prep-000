dc07627ab1e124deaf79d7f697b24370f88cbb84
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-15 21:22:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,16 +1,1 @@
-function forLoop(array) {
-  array = []
-  for (let i = 0; i < 25; i++) {
-      array.push( 
-        if (i = 0) {
-          return null
-        }
-        else if (i = 1) {
-          return 'I am ${i} strange loop.'
-        }
-        else if (i > 1) {
-          return 'I am ${i} strange loops.'
-        }
-      )
-  }
-}
+
