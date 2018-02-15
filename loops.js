6eecde325995fdbb87b80f0d77efa04f4d52a880
jackdyett@gmail.com Marston <jackdyett@gmail.com>
2018-02-15 21:35:24 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,6 @@ function forLoop(array) {
   array = []
   for (let i = 0; i < 25; i++) {
     if (i = 0) {
-      apple = 'laxative'
     }
     else if (i = 1) {
       array.push('I am 1 strange loop.')
